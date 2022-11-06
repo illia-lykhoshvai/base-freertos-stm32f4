748904a5b3b2bc78cfca56fca1debfabefa4999c
Illia Lykhoshvai <illia.lykhoshvai@gmail.com>
2022-11-06 10:31:43 +0200

    add cmsis_rtos_v2 files

--- Src/main.c
+++ Src/main.c
@@ -1,12 +1,30 @@
 #include "FreeRTOS.h"
+#include "task.h"
 
 #include "stm32f4xx.h"
 
+// TASKS
+void prvBlinkTask(void* pvParameters);
+
 int main(void) {
 
+    // init HW
+    RCC->AHB1ENR |= (1 << 2); // GPIOC en
+    GPIOC->MODER |= (1 << (13*2));
+    GPIOC->OTYPER |= ( 1 << 13 ); // PC13-LED
 
+    xTaskCreate(prvBlinkTask, "LED blink", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+
+    vTaskStartScheduler();
+
+    // should get here if no enough memory to create idle task
+    return 0;     
+}
+
+#define BLINK_DELAY ( ( TickType_t ) 500 / portTICK_PERIOD_MS )
+void prvBlinkTask(void* pvParameters) {
     while(1) {
-
+        GPIOC->ODR ^= (1 << 13);
+        vTaskDelay(BLINK_DELAY);
     }
-    
 }
