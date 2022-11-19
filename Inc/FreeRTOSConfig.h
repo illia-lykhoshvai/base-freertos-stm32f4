4c90294b218a107804621be776870698b543d55f
Illia Lykhoshvai <illia.lykhoshvai@gmail.com>
2022-11-19 13:58:52 +0200

    working copy

--- Inc/FreeRTOSConfig.h
+++ Inc/FreeRTOSConfig.h
@@ -51,8 +51,8 @@
 #if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
   #include <stdint.h>
   extern uint32_t SystemCoreClock;
-  void xPortSysTickHandler(void);
 #endif
+
 #ifndef CMSIS_device_header
 #define CMSIS_device_header "stm32f4xx.h"
 #endif /* CMSIS_device_header */
@@ -69,7 +69,7 @@
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 56 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE                    ((size_t)15360)
+#define configTOTAL_HEAP_SIZE                    ((size_t)5120)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configUSE_TRACE_FACILITY                 1
 #define configUSE_16_BIT_TICKS                   0
@@ -93,7 +93,7 @@
 #define configUSE_TIMERS                         1
 #define configTIMER_TASK_PRIORITY                ( 2 )
 #define configTIMER_QUEUE_LENGTH                 10
-#define configTIMER_TASK_STACK_DEPTH             256
+#define configTIMER_TASK_STACK_DEPTH             ( configMINIMAL_STACK_SIZE * 2 )
 
 /* CMSIS-RTOS V2 flags */
 #define configUSE_OS2_THREAD_SUSPEND_RESUME  1
@@ -160,6 +160,7 @@ header file. */
 standard names. */
 #define vPortSVCHandler    SVC_Handler
 #define xPortPendSVHandler PendSV_Handler
+#define xPortSysTickHandler SysTick_Handler
 
 /* IMPORTANT: After 10.3.1 update, Systick_Handler comes from NVIC (if SYS timebase = systick), otherwise from cmsis_os2.c */
 
--- Src/main.c
+++ Src/main.c
@@ -4,7 +4,7 @@
 #include "stm32f4xx.h"
 
 // TASKS
-void prvBlinkTask(void* pvParameters);
+static void prvBlinkTask(void* pvParameters);
 
 int main(void) {
 
@@ -13,17 +13,17 @@ int main(void) {
     GPIOC->MODER |= (1 << (13*2));
     GPIOC->OTYPER |= ( 1 << 13 ); // PC13-LED
 
-    xTaskCreate(prvBlinkTask, "LED blink", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    xTaskCreate(prvBlinkTask, "LED", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES-1, NULL);
 
     vTaskStartScheduler();
 
     // should get here if no enough memory to create idle task
-    return 0;     
+    return 0;
 }
 
-#define BLINK_DELAY ( ( TickType_t ) 500 / portTICK_PERIOD_MS )
-void prvBlinkTask(void* pvParameters) {
-    while(1) {
+#define BLINK_DELAY ( ( TickType_t ) 1000 / portTICK_PERIOD_MS )
+static void prvBlinkTask(void* pvParameters) {
+    for(;;) {
         GPIOC->ODR ^= (1 << 13);
         vTaskDelay(BLINK_DELAY);
     }
